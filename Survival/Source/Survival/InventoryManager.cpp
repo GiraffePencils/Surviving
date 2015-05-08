87c72c14cb093ffc00f08b2822936dfa798ef01a
GiraffePencils <giraffes_eat_pencils@hotmail.co.uk>
2015-05-09 00:13:38 +0100

    Created Inventory manager class

---
+++ Survival/Source/Survival/InventoryManager.cpp
@@ -0,0 +1,12 @@
+// This Code was written by Dean Lynch AKA GiraffePencils
+
+#include "Survival.h"
+#include "InventoryManager.h"
+
+InventoryManager::InventoryManager()
+{
+}
+
+InventoryManager::~InventoryManager()
+{
+}
---
+++ Survival/Source/Survival/InventoryManager.h
@@ -0,0 +1,13 @@
+// This Code was written by Dean Lynch AKA GiraffePencils
+
+#pragma once
+
+/**
+ * 
+ */
+class SURVIVAL_API InventoryManager
+{
+public:
+	InventoryManager();
+	~InventoryManager();
+};
