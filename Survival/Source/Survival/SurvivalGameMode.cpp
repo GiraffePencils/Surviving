e95c8a20a5e5584bbdc69abb457a721e6129de26
GiraffePencils <giraffes_eat_pencils@hotmail.co.uk>
2015-06-03 23:05:04 +0100

    Custom Character created, need to move code from bp

--- Survival/Source/Survival/SurvivalGameMode.cpp
+++ Survival/Source/Survival/SurvivalGameMode.cpp
@@ -8,7 +8,7 @@ ASurvivalGameMode::ASurvivalGameMode(const FObjectInitializer& ObjectInitializer
 	: Super(ObjectInitializer)
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/SurvivalThirdPersonChar"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/SThirdPersonChar"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
